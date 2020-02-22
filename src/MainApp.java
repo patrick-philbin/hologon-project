04b4b05fbe4ba15dbabdcd296bc6f335ee333a3e
patrick-philbin <patrick@philbin.net>
2020-02-22 14:10:15 -0500

    Nice, pretty menus for all of your file changing needs

--- src/MainApp.java
+++ src/MainApp.java
@@ -6,6 +6,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -21,6 +23,11 @@ import java.io.File;
 
 
 public class MainApp extends Application {
+
+    private int screenWidth;
+    private int screenHeight;
+    private int baseSquare;
+    private String path;
 
     public static void main(String[] args) {
         launch(args);
@@ -39,12 +46,27 @@ public class MainApp extends Application {
         HBox toolbar = new HBox();
         toolbar.getChildren().add(new Text("This is a toolbar, lol"));
 
+        VBox fileTypeHolder = new VBox();
+        fileTypeHolder.setAlignment(Pos.CENTER);
+        fileTypeHolder.getChildren().add(new Text("File Type"));
+        ToggleButton videoChoice = new ToggleButton("Video");
+        videoChoice.setUserData("Video");
+        ToggleButton modelChoice = new ToggleButton("3D Model");
+        modelChoice.setUserData("Model");
+        ToggleGroup fileType = new ToggleGroup();
+        videoChoice.setToggleGroup(fileType);
+        modelChoice.setToggleGroup(fileType);
+        HBox toggleHolder = new HBox();
+        toggleHolder.setAlignment(Pos.CENTER);
+        toggleHolder.getChildren().addAll(videoChoice, modelChoice);
+        fileTypeHolder.getChildren().add(toggleHolder);
+
         VBox resolution = new VBox();
         resolution.getChildren().add(new Text("Resolution"));
         HBox resEntry = new HBox();
         resEntry.setAlignment(Pos.CENTER);
-        TextField screenWidthPx = new TextField();
-        TextField screenHeightPx = new TextField();
+        TextField screenWidthPx = new TextField("1600");
+        TextField screenHeightPx = new TextField("1200");
         screenWidthPx.setAlignment(Pos.BASELINE_RIGHT);
         resEntry.getChildren().addAll(screenWidthPx, new Text("x"), screenHeightPx, new Text("px"));
         resolution.getChildren().add(resEntry);
@@ -59,29 +81,44 @@ public class MainApp extends Application {
         innerBox.setAlignment(Pos.CENTER);
 
         VBox videoBox = new VBox();
-        videoBox.getChildren().add(new Text("Video Filepath"));
-        TextField videopath = new TextField("STL/WayfindersCoin.stl");
-        videopath.setMaxWidth(350);
-        videoBox.getChildren().add(videopath);
+        videoBox.getChildren().add(new Text("Filepath"));
+        TextField filepath = new TextField("No File Type Selected!");
+        filepath.setEditable(false);
+        filepath.setMaxWidth(350);
+        videoBox.getChildren().add(filepath);
         videoBox.setAlignment(Pos.CENTER);
 
         Button launch = new Button("Launch");
 
-        menuElements.getChildren().addAll(resolution, innerBox, videoBox, launch);
+        menuElements.getChildren().addAll(fileTypeHolder, resolution, innerBox, videoBox, launch);
         base.setTop(toolbar);
         base.setCenter(menuElements);
         Scene menu = new Scene(base, 500, 500);
 
-        screenWidthPx.setText("1600");
-        screenHeightPx.setText("1200");
+        //Setting toggle changes for filetype
+        fileType.selectedToggleProperty().addListener((ov, toggle, new_toggle) -> {
+            if(new_toggle == null){
+                filepath.setEditable(false);
+                filepath.setText("No File Type Selected!");
+            }
+            else if(new_toggle.getUserData().equals("Video")){
+                filepath.setEditable(true);
+                filepath.setText("videos/fishes.mp4");
+            }
+            else if(new_toggle.getUserData().equals("Model")){
+                filepath.setEditable(true);
+                filepath.setText("STL/WayfindersCoin.stl");
+            }
+        });
 
         //Creating the second stage for viewing the video
         launch.setOnAction(event -> {
             Stage secondaryStage = new Stage();
-            Viewer.setScreenWidth(Integer.parseInt(screenWidthPx.getText()));
-            Viewer.setScreenHeight(Integer.parseInt(screenHeightPx.getText()));
-            Viewer.setBase(Integer.parseInt(innerBoxSize.getText()));
-            Scene videoView = Viewer.Model(videopath.getText());
+            screenWidth = Integer.parseInt(screenWidthPx.getText());
+            screenHeight = Integer.parseInt(screenHeightPx.getText());
+            baseSquare = Integer.parseInt(innerBoxSize.getText());
+
+            Scene videoView = buildViewer((String)fileType.getSelectedToggle().getUserData(), filepath.getText());
             videoView.setOnKeyPressed(e -> {
                 if(e.getCode().equals(KeyCode.ESCAPE)){
                     secondaryStage.close();
@@ -105,4 +142,20 @@ public class MainApp extends Application {
         primaryStage.show();
 
     }
+
+    private Scene buildViewer(String type, String path){
+        Viewer.setScreenWidth(screenWidth);
+        Viewer.setScreenHeight(screenHeight);
+        Viewer.setBase(baseSquare);
+        if(type.equals("Video")){
+            return Viewer.Video(path);
+        }
+        else if(type.equals("Model")){
+            return Viewer.Model(path);
+        }
+        else{
+            return null;
+        }
+    }
+
 }
