00ea024008d0c63e35b2e5d41ff90fecb0eb0e2c
Daniel Masker <madly123iamme@gmail.com>
2020-02-22 01:24:42 -0500

    Updated Viewer

--- src/Conversion/Viewer.java
+++ src/Conversion/Viewer.java
@@ -1,12 +1,15 @@
 package Conversion;
 
 import javafx.scene.Scene;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import javax.swing.text.View;
 
 public class Viewer {
-    static int height;
+    private static int screenHeight, screenWidth, baseHeight, baseWidth;
 
-    public static Scene Mathematica(int screenWidth, int screenHeight, int baseWidth, int baseHeight) {
-        return null;
+    public static Scene Mathematica() {
+        throw new NotImplementedException();
     }
 
     public static Scene Video() {
@@ -14,6 +17,18 @@ public class Viewer {
     }
 
     public static void setScreenWidth(int width) {
+        Viewer.screenWidth = width;
+    }
 
+    public static void setScreenHeight(int height) {
+        Viewer.screenHeight = height;
+    }
+
+    public static void setBaseHeight(int height) {
+        Viewer.baseHeight = height;
+    }
+
+    public static void setBaseWidth(int width) {
+        Viewer.baseWidth = width;
     }
 }
