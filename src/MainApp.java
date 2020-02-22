486f1039e5cf0f3e7800fda9645ec4670e9357be
patrick-philbin <patrick@philbin.net>
2020-02-22 02:47:57 -0500

    Added inner box size to the menu

--- src/MainApp.java
+++ src/MainApp.java
@@ -50,6 +50,14 @@ public class MainApp extends Application {
         resolution.getChildren().add(resEntry);
         resolution.setAlignment(Pos.CENTER);
 
+        VBox innerBox = new VBox();
+        innerBox.getChildren().add(new Text("Inner Box Size"));
+        TextField innerBoxSize = new TextField("300");
+        innerBoxSize.setMaxWidth(100);
+        innerBoxSize.setAlignment(Pos.CENTER);
+        innerBox.getChildren().add(innerBoxSize);
+        innerBox.setAlignment(Pos.CENTER);
+
         VBox videoBox = new VBox();
         videoBox.getChildren().add(new Text("Video Filepath"));
         TextField videopath = new TextField("videos/fishes.mp4");
@@ -59,16 +67,17 @@ public class MainApp extends Application {
 
         Button launch = new Button("Launch");
 
-        menuElements.getChildren().addAll(resolution, videoBox, launch);
+        menuElements.getChildren().addAll(resolution, innerBox, videoBox, launch);
         base.setTop(toolbar);
         base.setCenter(menuElements);
         Scene menu = new Scene(base, 500, 500);
 
         //Creating the second stage for viewing the video
-
-
         launch.setOnAction(event -> {
             Stage secondaryStage = new Stage();
+            Viewer.setScreenWidth(Integer.parseInt(screenWidthPx.getText()));
+            Viewer.setScreenHeight(Integer.parseInt(screenHeightPx.getText()));
+            Viewer.setBase(Integer.parseInt(innerBoxSize.getText()));
             secondaryStage.setScene(Viewer.Video(videopath.getText()));
             secondaryStage.setFullScreen(true);
             secondaryStage.show();
