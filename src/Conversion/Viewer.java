6cd7740b47da0b68710c8d206f16b9583c3638db
Daniel Masker <madly123iamme@gmail.com>
2020-02-22 01:14:49 -0500

    Added Viewer

--- src/Conversion/Viewer.java
+++ src/Conversion/Viewer.java
@@ -3,11 +3,15 @@ package Conversion;
 import javafx.scene.Scene;
 
 public class Viewer {
-    public static Scene Mathematica() {
+    public static Scene Mathematica(int screenWidth, int screenHeight, int baseWidth, int baseHeight) {
         return null;
     }
 
     public static Scene Video() {
         return null;
+    }
+
+    public static void setScreenWidth(int width) {
+
     }
 }
