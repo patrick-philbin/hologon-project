3f9603a054a0b1972bf2dc028bb53daa58aeb28e
Daniel Masker <madly123iamme@gmail.com>
2020-02-22 01:12:25 -0500

    Added Viewer

---
+++ src/Conversion/Viewer.java
@@ -0,0 +1,13 @@
+package Conversion;
+
+import javafx.scene.Scene;
+
+public class Viewer {
+    public static Scene Mathematica() {
+        return null;
+    }
+
+    public static Scene Video() {
+        return null;
+    }
+}
